fcf701bcb5fd46cc2386761cec35d3e87bc1c527
saahiljain <saajain1@publicisgroupe.com>
2020-04-11 10:31:25 +0530

    Update models.py

--- src/models.py
+++ src/models.py
@@ -125,7 +125,7 @@ class BaseModel:
             name = os.path.basename(img_gray_path)
             path = os.path.join(outputs_path, name)
 
-            feed_dic = {self.input_gray: img_gray[None, :, :, None]}
+            feed_dic = {self.input_gray: img_gray[None, :, :, 1]}
             print("feed_dic",feed_dic)
             outputs = self.sess.run(self.sampler, feed_dict=feed_dic)
             outputs = postprocess(tf.convert_to_tensor(outputs), colorspace_in=self.options.color_space, colorspace_out=COLORSPACE_RGB).eval() * 255
